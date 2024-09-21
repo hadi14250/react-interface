97a35c2ddd11ee2fe0627d1970a5f0b2708d0070
hadi14250 <hadihk2k@gmail.com>
2024-09-21 19:26:46 +0400

    installed tailwind css and configured it to work with the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BsTrash3 } from "react-icons/bs";
+import { PiPoliceCarFill } from "react-icons/pi";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
         Hello World
       </h1>
         <BsTrash3 />
+        <PiPoliceCarFill />
     </div>
   );
 }
