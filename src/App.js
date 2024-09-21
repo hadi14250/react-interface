1fd3e43a36ec5cc148cd335e2ead139e1a30c886
hadi14250 <hadihk2k@gmail.com>
2024-09-21 20:39:53 +0400

    fixed outdated tailwind colors

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
           .map(appointment => (
             <li className="px-3 py-3 flex items-start">
               <button type="button"
-                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red hover:bg-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue">
                 <BiTrash /></button>
               <div className="flex-grow">
                 <div className="flex items-center">
-                  <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
+                  <span className="flex-none font-medium text-2xl text-blue">{appointment.petName}</span>
                   <span className="flex-grow text-right">{appointment.aptDate}</span>
                 </div>
-                <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
+                <div><b className="font-bold text-blue">Owner:</b> {appointment.ownerName}</div>
                 <div className="leading-tight">{appointment.aptNotes}</div>
               </div>
             </li>
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,19 +6,19 @@ const DropDown = () => {
         rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
         <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
           <div
-            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            className="px-4 py-2 text-sm text-gray hover:bg-gray hover:text-gray flex justify-between cursor-pointer"
             role="menuitem">Pet Name <BiCheck /></div>
           <div
-            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            className="px-4 py-2 text-sm text-gray hover:bg-gray hover:text-gray flex justify-between cursor-pointer"
             role="menuitem">Owner Name  <BiCheck /></div>
           <div
-            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            className="px-4 py-2 text-sm text-gray hover:bg-gray hover:text-gray flex justify-between cursor-pointer"
             role="menuitem">Date <BiCheck /></div>
           <div
-            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+            className="px-4 py-2 text-sm text-gray hover:bg-gray hover:text-gray flex justify-between cursor-pointer border-gray-1 border-t-2"
             role="menuitem">Asc <BiCheck /></div>
           <div
-            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+            className="px-4 py-2 text-sm text-gray hover:bg-gray hover:text-gray flex justify-between cursor-pointer"
             role="menuitem">Desc <BiCheck /></div>
         </div>
       </div>
@@ -35,11 +35,11 @@ const Search = ()=> {
                   <label htmlFor="query" className="sr-only" />
                 </div>
                 <input type="text" name="query" id="query" value=""
-                  className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
+                  className="pl-8 rounded-md focus:ring-indigo focus:border-indigo block w-full sm:text-sm border-gray" placeholder="Search" />
                 <div className="absolute inset-y-0 right-0 flex items-center">
                   <div>
                     <button type="button"
-                      className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
+                      className="justify-center px-4 py-2 bg-blue border-2 border-blue text-sm text-white hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                       Sort By <BiCaretDown className="ml-2" />
                     </button>
                     <DropDown />
