dc2c16671c89401a8b56d4aa75a16c555f8a2d30
hadi14250 <hadihk2k@gmail.com>
2024-09-21 19:34:47 +0400

    tested with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BsTrash3 } from "react-icons/bs";
-import { PiPoliceCarFill } from "react-icons/pi";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
+        <BsTrash3 className="inline-block align-top" style={{ color: 'red' }} />
         Hello World
       </h1>
-        <BsTrash3 />
-        <PiPoliceCarFill />
     </div>
   );
 }
