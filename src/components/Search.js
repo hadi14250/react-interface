8d934d073aa8f9e567f7141f9e5d7f7594ce2a4a
hadi14250 <hadihk2k@gmail.com>
2024-09-22 15:40:34 +0400

    used UseState, UseEffect and useCallback to  fetch and render data, the reason I used useCallback is to define the fetchData function and so I dont have to rewrite the same function whenever I want to use useEffect to fetch

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,10 @@
-import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi"
+import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi";
+import { useState } from "react";
 
-const DropDown = () => {
+const DropDown = ( {dropDown} ) => {
+  if (!dropDown)
+      return(null)
+  else
     return (
       <div className="origin-top-right absolute right-0 mt-2 w-56
         rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -27,6 +31,9 @@ const DropDown = () => {
   
 
 const Search = ()=> {
+
+    let [dropDown, setDropDown] = useState(false)
+
         return (
             <div className="py-5">
               <div className="mt-1 relative rounded-md shadow-sm">
@@ -38,11 +45,16 @@ const Search = ()=> {
                   className="pl-8 rounded-md focus:ring-indigo focus:border-indigo block w-full sm:text-sm border-gray" placeholder="Search" />
                 <div className="absolute inset-y-0 right-0 flex items-center">
                   <div>
-                    <button type="button"
-                      className="justify-center px-4 py-2 bg-blue border-2 border-blue text-sm text-white hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
+                    <button
+                    type="button"
+                    className="justify-center px-4 py-2 bg-blue border-2 border-blue text-sm text-white hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true"
+                    onClick={
+                      () => setDropDown(!dropDown)
+                    }
+                    >
                       Sort By <BiCaretDown className="ml-2" />
                     </button>
-                    <DropDown />
+                    <DropDown dropDown={dropDown} />
                   </div>
                 </div>
               </div>
