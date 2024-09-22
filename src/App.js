cc18fb974dffd111c0f7753dc13f5c4d3246b474
hadi14250 <hadihk2k@gmail.com>
2024-09-22 15:55:41 +0400

    added delete functionality

--- src/App.js
+++ src/App.js
@@ -34,6 +34,11 @@ function App() {
             < AppointmentInfo
               key={appointment.id}
               appointment={appointment}
+              onDeleteAppointment={
+                appointmentId => 
+                  setAppointmentList(appointmentList.filter(appointment =>
+                    appointment.id !== appointmentId))
+              }
             />
           ))
         }
--- src/components/AppointmentInfo.js
+++ src/components/AppointmentInfo.js
@@ -1,11 +1,16 @@
 import { BiTrash } from "react-icons/bi";
 
-const AppointmentInfo = ( { appointment } ) => {
+const AppointmentInfo = ( { appointment, onDeleteAppointment } ) => {
     return (
             <li className="px-3 py-3 flex items-start">
-            <button type="button"
-            className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red hover:bg-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue">
-            <BiTrash /></button>
+            <button
+            onClick={ () => onDeleteAppointment(appointment.id) }
+            type="button"
+            className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red hover:bg-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue"
+            >
+            <BiTrash />
+            </button>
+
             <div className="flex-grow">
             <div className="flex items-center">
                 <span className="flex-none font-medium text-2xl text-blue">{appointment.petName}</span>
